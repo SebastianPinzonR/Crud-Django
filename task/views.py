05c9a3333139cd2fe93fa0622768bb36a3cd3113
Sebastian <spinzonrojas@gmail.com>
2025-08-05 12:06:49 -0500

    se pusieron las funcionalidades para crear las tareas

--- task/views.py
+++ task/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
 
 # Create your views here.
 
 def list_tasks(request):
     return render(request, 'list_task.html')
+
+def create_task(request):
+    #aqui no se castea el request.POST
+    #aqui se puede validar si lanza una excepcion o extenderlo a otra vista
+    Task(title = request.POST['title'],
+         description = request.POST['description']).save()
+    #aqui se castea el request.POST
+    # task= Task(title = request.POST['title'],
+    #      description = request.POST['description']).save()
+    # task.save()
+    return redirect('/tasks/')
